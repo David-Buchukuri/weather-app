65282a8056f3ca6e41e4f14c9ebe5f3a7e12f1e4
davit buchukuri <datobuchukuri20@gmail.com>
2022-03-22 15:37:52 +0400

    apis set up

--- src/App.js
+++ src/App.js
@@ -10,9 +10,33 @@ let array = [
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const fetchCur = () => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=tbilisi&units=metric&appid=5f5fced40cc62d51499dacc3c0ca8151" //standard
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   };
+
+  const fetchForcast = () => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=41.7151&lon=44.8271&units=metric&appid=5f5fced40cc62d51499dacc3c0ca8151" //onecall
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  const fetchGeo = () => {
+    fetch(
+      "http://api.openweathermap.org/geo/1.0/direct?q=tbilisi&limit=1&appid=5f5fced40cc62d51499dacc3c0ca8151"
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  // const options = {weekday:'long',year:'numeric',month:'long',day:'numeric'}
+  // let t = new Date(1648112400 * 1000).toLocaleTimeString("en-US",options)
+  // console.log(t);
 
   useEffect(() => {
     let timeout = setTimeout(() => {
@@ -24,13 +48,15 @@ function App() {
 
   return (
     <div className="App">
-      {array.map((el) => (
-        <div>
+      {array.map((el, idx) => (
+        <div key={idx}>
           <DropBtn data={el} />
         </div>
       ))}
 
-      <button onClick={handleClick}></button>
+      <button onClick={fetchCur}>fetch current</button>
+      <button onClick={fetchGeo}>fetch geo</button>
+      <button onClick={fetchForcast}>fetch forecast</button>
     </div>
   );
 }
